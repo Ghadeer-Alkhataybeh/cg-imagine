c2a30e17bc381443906191162424473153a2da45
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2019-03-24 20:24:15 +0200

    <feat> MongoDB connected VIA mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  monogoURI: "mongodb://cg:gg835864@ds121996.mlab.com:21996/cg"
+};
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//DB config
+const db = require("./config/keys").monogoURI;
+
+//Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Erorr", err));
 
 //simple route to get sth to run
 app.get("/", (req, res) => res.send("Ghadeer"));
