4f92ec199585e97590b2f1a41c38b3a202ca7630
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2019-03-24 23:19:09 +0200

    <feat>User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,41 @@
 //everything related to authentication will be here
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
 
 // testing users route
 // access Public
 router.get("/test", (req, res) => res.json({ name: "users is working" }));
+
+// Register users route
+// access Public
+router.post("/register", (req, res) => {
+  //checking if we already have the user in the DB
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res
+        .status(400)
+        .json({ email: "Email already have been registered" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
